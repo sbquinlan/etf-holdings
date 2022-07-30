178819fb4546154110bdfc6fa175b102f3f80bd4
Sean Quinlan <1011062+sbquinlan@users.noreply.github.com>
2022-07-29 18:06:02 -0700

    it works now, but still can't get a brokerage session

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { stat } from 'fs';
+import { posix } from 'path';
 import { paths } from './ibweb'
 
 /**
@@ -7,6 +9,13 @@ import { paths } from './ibweb'
  * In OpenAPI 2, this has a 'schema' field that maps our response type
  * In OpenAPI 3, this has a 'content'-type field that maps to our response type
  */
+type ApiPathArgs<OP> = OP extends { 
+  // openapi 2
+  parameters?: { path? : infer P, query?: infer Q, body?: infer B },
+  // openapi 3
+  requestBody?: { content: { 'application/json': infer RB } }
+} ? P & Q & B & RB
+  : undefined;
 type ApiResponseSchema<OP> = OP extends { responses: infer R } 
   ? { 
       [S in keyof R]: R[S] extends { schema?: infer S } // openapi 2
@@ -22,13 +31,39 @@ type ApiStatusSchema<TResponseSchema> = 200 extends keyof TResponseSchema
   ? TResponseSchema[200] 
   : unknown;
 
+function form_path(path: string, args?: Record<string, string>, method: string) {
+  if (!args) {
+    return { path, body: undefined };
+  }
+
+  const search_params = new URLSearchParams();
+  for (const [key, value] of Object.entries(args)) {
+    const search = `{${key}}`;
+    if (path.indexOf(search)) {
+      path = String(path).replace(search, String(value))
+    } else {
+      search_params.set(key, value);
+    }
+  }
+  return { 
+    path: method === 'get' ? path + '?' + search_params : path,
+    body: method !== 'get' ? JSON.stringify(Object.fromEntries(search_params.entries())) : undefined,
+  }
+}
+
 function api<TApi>({ base } : { base: string }) {
   async function typedFetch<
     TPath extends keyof TApi, 
     TMethod extends keyof TApi[TPath],
     TResult extends ApiStatusSchema<ApiResponseSchema<TApi[TPath][TMethod]>>,
-  >(path: TPath, init: { method: TMethod } & RequestInit): Promise<TResult> {
-    const resp = await fetch(`${base}${String(path)}`, init)
+  >(fpath: TPath, init: { method: TMethod } & RequestInit, args?: ApiPathArgs<TApi[TPath][TMethod]>): Promise<TResult> {
+    // try to replace path args
+    const { path, body } = form_path(
+      String(fpath), 
+      args as Record<string, string> | undefined,
+      init.method
+    );
+    const resp = await fetch(`${base}${path}`, { ... init, body })
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
@@ -52,7 +87,7 @@ class AuthHeartbeat {
 
   start() {
     if (this._timeout) return;
-    this._schedule();
+    this._heartbeat().then(() => this._schedule())
   }
 
   stop() {
@@ -74,7 +109,7 @@ class AuthHeartbeat {
   private async _heartbeat() {
     const { authenticated, connected, fail } = await ib_fetch(
       '/iserver/auth/status', 
-      { method: 'post' }
+      { method: 'post' },
     );
     if (fail) {
       this._throwError(<string>fail);
@@ -101,8 +136,11 @@ async function main() {
   const heartbeat = new AuthHeartbeat(60000);
   heartbeat.start();
 
-  const resp = await ib_fetch('/portfolio/accounts', { method: 'get' });
-  console.log(resp)
+  // const accounts = await ib_fetch('/portfolio/accounts', { method: 'get' });
+  // const acctId = accounts && accounts[0].accountId;
+  // const summary = await ib_fetch('/portfolio/{accountId}/summary', { method: 'get' }, { accountId: String(acctId) });
+  // const trades = await ib_fetch('/iserver/account/trades', { method: 'get'});
+//  console.log(trades);
 }
 
 main()
