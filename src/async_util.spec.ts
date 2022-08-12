15af80e97d98269ca193c34a1d3e03b769b92091
Sean Quinlan <1011062+sbquinlan@users.noreply.github.com>
2022-08-12 14:47:45 -0700

    wrote some more utils, committing for notes

--- src/async_util.spec.ts
+++ src/async_util.spec.ts
@@ -1,4 +1,4 @@
-import { map, sleep, to_array, any_va, LeakyIterator } from './async_util';
+import { AsyncNode, sleep, to_array, any_va } from './async_util';
 
 async function* asyncRange(n: number) {
   for (let i = 0; i < n; i++) yield i;
@@ -43,41 +43,55 @@ describe('any_va', () => {
   })
 })
 
-describe('rate_limit', () => {
-  it('should work with 3', async () => {
-    const iter = asyncRange(10)[Symbol.asyncIterator]();
-    const result = await to_array(
-      map(
-        new LeakyIterator(
-          iter,
-          3,
-          100
-        ), 
-        async (x) => {
-          await sleep(100)
-          return x 
-        },
-        3,
-      )
-    );
-    expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  });
+// describe('rate_limit', () => {
+//   it('should work with 3', async () => {
+//     const iter = asyncRange(10)[Symbol.asyncIterator]();
+//     const result = await to_array(
+//       map(
+//         new LeakyIterator(
+//           iter,
+//           3,
+//           100
+//         ), 
+//         async (x) => {
+//           await sleep(100)
+//           return x 
+//         },
+//         3,
+//       )
+//     );
+//     expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+//   });
 
-  it('should work with 1', async () => {
-    const iter = asyncRange(10)[Symbol.asyncIterator]();
-    const result = await to_array(
-      map(
-        new LeakyIterator(
-          iter,
-          3,
-          100
-        ), 
-        async (x) => {
-          await sleep(100)
-          return x 
-        },
-      )
-    );
-    expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+//   it('should work with 1', async () => {
+//     const iter = asyncRange(10)[Symbol.asyncIterator]();
+//     const result = await to_array(
+//       map(
+//         new LeakyIterator(
+//           iter,
+//           3,
+//           100
+//         ), 
+//         async (x) => {
+//           await sleep(100)
+//           return x 
+//         },
+//       )
+//     );
+//     expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+//   });
+// });
+
+describe('AsyncNode', () =>  {
+  it('should work', async () => {
+    let tail = new AsyncNode(0);
+    const iter = tail[Symbol.asyncIterator]();
+
+    tail = tail.setNext(new AsyncNode(1));
+    expect(await iter.next()).toEqual({ value: 0, done: false });
+    expect(await iter.next()).toEqual({ value: 1, done: false });
+    const paused = iter.next();
+    tail = tail.setNext(new AsyncNode(2))
+    expect(await paused).toEqual({ value: 2, done: false });
   });
 });
